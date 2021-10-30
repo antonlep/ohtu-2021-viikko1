e818a59355076f6664c5053ed9663864b6b0a770
antonlep <anton@FIL16975W.accdom.for.int>
2021-10-30 11:43:17 +0300

    Add testing

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -13,6 +13,14 @@ class TestVarasto(unittest.TestCase):
     def test_uudella_varastolla_oikea_tilavuus(self):
         self.assertAlmostEqual(self.varasto.tilavuus, 10)
 
+    def test_uusi_varasto_ei_voi_olla_negatiivinen(self):
+        varasto = Varasto(-10)
+        self.assertAlmostEqual(varasto.tilavuus, 0)
+
+    def test_alkusaldo_ei_voi_olla_negatiivinen(self):
+        varasto = Varasto(10,-10)
+        self.assertAlmostEqual(varasto.saldo, 0)
+
     def test_lisays_lisaa_saldoa(self):
         self.varasto.lisaa_varastoon(8)
 
@@ -23,6 +31,28 @@ class TestVarasto(unittest.TestCase):
 
         # vapaata tilaa pitäisi vielä olla tilavuus-lisättävä määrä eli 2
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 2)
+
+    def test_ei_voi_lisata_negatiivista_maaraa(self):
+        self.varasto.lisaa_varastoon(-1)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_ei_voi_lisata_enemman_kuin_mahtuu(self):
+        self.varasto.lisaa_varastoon(12)
+
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_ei_voi_ottaa_negatiivista_maaraa(self):
+        maara = self.varasto.ota_varastosta(-1)
+
+        self.assertAlmostEqual(maara, 0)
+
+    def test_liikaa_ottaminen_ottaa_kaikki_mita_voidaan(self):
+        self.varasto.lisaa_varastoon(8)
+        
+        maara = self.varasto.ota_varastosta(12)
+
+        self.assertAlmostEqual(maara, 8)
 
     def test_ottaminen_palauttaa_oikean_maaran(self):
         self.varasto.lisaa_varastoon(8)
@@ -38,3 +68,11 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_tulostaa_oikein(self):
+        self.varasto.lisaa_varastoon(8)
+
+        self.varasto.ota_varastosta(2)
+
+        # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
+        self.assertAlmostEqual(str(self.varasto), "saldo = 6, vielä tilaa 4")
